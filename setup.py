a9dc391e48093d3cee5875209d28ceda901eb594
Steven Pak <69532598+Pakman450@users.noreply.github.com>
2022-08-25 18:14:41 -0400

    build: edit License file and add setup.py for PIP package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='mol2db',
+    version='0.1.0',    
+    description='A molecule library handeler',
+    url='https://github.com/Pakman450/mol2db',
+    author='Steven Pak',
+    author_email='stevemn.pak10@gmail.com',    
+    license='MIT license',
+    packages=['mol2db'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)  
