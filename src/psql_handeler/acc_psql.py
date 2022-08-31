1326795b401737a62534290dfbb07bfec7e50775
Steven Pak <steven.pak10@gmail.com>
2022-08-31 15:11:23 -0400

    fix: edit psql connect function

--- src/psql_handeler/acc_psql.py
+++ src/psql_handeler/acc_psql.py
@@ -4,12 +4,15 @@ import psycopg
 
 
 
-#def connect2psql (db_name,user_name,pw,ht,prt):
-def connect2psql (db_name,user_name,pw):
+def connect2psql (db_name="",user_name="",pw="",ht="",prt=""):
     print("lol1")
-    conn = psycopg.connect(dbname=db_name, user = user_name, password = pw)
+    conn = psycopg.connect(dbname=db_name, user = user_name, password = pw,host = ht, port = prt)
     print("Opened database successfully")
     print("lol")
 
 
-connect2psql("spak","spak","KapSat2400180000")
+
+
+
+#connect2psql("spak","spak","KapSat2400180000","/var/run/postgresql","5432")
+connect2psql("spak")
