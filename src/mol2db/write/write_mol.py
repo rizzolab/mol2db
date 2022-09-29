6c3b4d3765a6e2cb391bc1e3ee1d1ef63a0709ae
Steven Pak <steven.pak10@gmail.com>
2022-09-29 13:22:40 -0400

    feat: add comments on write_mol.py

--- src/mol2db/write/write_mol.py
+++ src/mol2db/write/write_mol.py
@@ -1,20 +1,10 @@
 
-
 def write_mol(output_name,mol):
 
     output_name = 'output.mol2' 
 
     with open(output_name,'a') as write_file: 
-        #write_file.write('@<TRIPOS>MOLECULE\n')
-        #write_file.write(mol.Name+'\n')
-        #write_file.write(' ' + str(len(mol.atoms["atom_num"])) + " " + str(len(mol.bonds["bond_num"])) + ' 1 0 0\n')
-        #write_file.write('SMALL\n')
-        #write_file.write('USER_CHARGES\n')
-        #write_file.write('\n')
-        #write_file.write('@<TRIPOS>ATOM\n')
-
-
-
+        #using f string to fill the atom_string
         atom_string = f'' 
         for num in range(0,mol.num_atoms):
             atom_string += f"{mol.atoms['atom_num'][num]:{7}} "
@@ -50,16 +40,5 @@ def write_mol(output_name,mol):
             '@<TRIPOS>SUBSTRUCTURE\n'
             +'     1 <0>         1 TEMP              0 ****  ****    0 ROOT\n'
         )
-
-
-
-
-
-
-        
+        #write the mol in a mol2 format
         write_file.write(mol_string) 
-            
-        
-
-
-
