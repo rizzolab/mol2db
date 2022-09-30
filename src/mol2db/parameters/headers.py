b91ab0b859a2de71dc060d26794b6c56ace3013b
Steven Pak <steven.pak10@gmail.com>
2022-09-30 12:53:08 -0400

    feat: add parameters folder with header definitions

---
+++ src/mol2db/parameters/headers.py
@@ -0,0 +1,23 @@
+# you can set your expected header information hear  
+header_dict = {
+        "name"           :      '',
+        "mw"             :      0.0,
+        "rot_bond"       :      0,
+        "charge"         :      0.0,
+        "hba"            :      0,
+        "hbd"            :      0,
+        "heavy_atoms"    :      0,
+        "num_atom_rings" :      0,
+        "num_alip_rings" :      0,
+        "num_sat_rings"  :      0,
+        "num_stereo"     :      0,
+        "num_spiro"      :      0,
+        "logp"           :      0.0,
+        "tpsa"           :      0.0,
+        "syntha"         :      0.0,
+        "qed"            :      0.0,
+        "logs"           :      0.0,
+        "num_pain"       :      0.0,
+        "pains_names"    :      '',
+        "smiles"         :      ''
+}
