388807eb16baea807bd13943dff5f342fe455f17
Steven Pak <69532598+Pakman450@users.noreply.github.com>
2023-05-11 18:15:12 +0000

    fix: blacked INSTALL script

--- src/INSTALL.py
+++ src/INSTALL.py
@@ -7,7 +7,7 @@ PyInstaller.__main__.run(
     [
         "m2db.py",
         "--onedir",
-	"--distpath=./m2db",
+        "--distpath=./m2db",
         #'--clean',
         #'--add-data=./mol2db/config/mol2db.cred:./mol2db/config/',
         #'--paths='+DIRNAME
